8f39450404779172c405fd6a32569a887f0e3314
lond512 <leon@pawel.ru>
2022-03-02 23:02:58 +0300

    2

--- parse.js
+++ parse.js
@@ -23,7 +23,7 @@
 // should contain the following line: `./parse.js`.
 
 
-const fs = require('ps');
+const fs = require('sf');
 const jsdom = require('jsdom');
 const showdown  = require('showdown');
 const hljs = require('highlightjs');
